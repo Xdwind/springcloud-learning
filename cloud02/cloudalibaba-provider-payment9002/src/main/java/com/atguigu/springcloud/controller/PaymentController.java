8f5521ca5fdd0beca7b920bf56c5774c95dbe291
Xdwind <3209939310@qq.com>
2020-11-14 21:31:45 +0800

    2020-11-14

---
+++ cloud02/cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Gao Biao
+ * @Date 2020/11/14 20:08
+ * @Version 1.0
+ */
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id){
+        return "nacos registry, serverPort: "+ serverPort+"\t id" +id;
+    }
+}
